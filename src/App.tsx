425626c947e745443ca432af2b40c457ae735341
srp0114 <ksy010114@gmail.com>
2023-03-26 00:18:21 +0900

    FEAT : #2 추가정보 UI 구현 - /welcome 라우팅 - 닉네임, 관심기술, 자기소개 useState 이용 - 아이디, 학번, 트랙 임시 데이터(TestUserAccount)로 지정, disabled 처리 - 관심기술 데이터 - 앞서 만들어둔 언어 데이터 활용 - 프로필 선택 작업 진행중

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
-import BoardWrite from "./components/pages/PostForm";
+import PostForm from "./components/pages/PostForm";
 import FreeBoard from "./components/pages/Board/Free/FreeBoard";
 import FreeDetails from "./components/pages/Board/Free/FreeDetails";
 import QnABoard from "./components/pages/Board/QnA/QnABoard";
@@ -11,6 +11,7 @@ import RecruitDetails from "./components/pages/Board/Recruit/RecruitDetails";
 import MyPage from "./components/pages/MyPage";
 import Notice from "./components/pages/Notice";
 import OAuth2 from "./components/pages/OAuth2";
+import Welcome from "./components/pages/Welcome";
 
 function App() {
   return (
@@ -18,7 +19,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="post" element={<BoardWrite />} />
+          <Route path="post" element={<PostForm />} />
           <Route path="/free" element={<FreeBoard />} />
           <Route path="/free/:id" element={<FreeDetails />} />
           <Route path="/questions" element={<QnABoard />} />
@@ -29,6 +30,7 @@ function App() {
           <Route path="/notice" element={<Notice />} />
           <Route path="/redirect" element={<OAuth2 />} />
           <Route path="/authorized" element={<OAuth2 />} />
+          <Route path="/welcome" element={<Welcome/>}/>
         </Routes>
       </Router>
     </>
