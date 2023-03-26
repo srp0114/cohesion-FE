9195f11b0f373d445aae292829d44034eb5fa5a8
srp0114 <ksy010114@gmail.com>
2023-03-26 11:01:02 +0900

    FEAT : #2 - 추가정보 UI 추가 구현 - 프로필 선택 UI - 관심기술 핸들러 수정 - css 수정

--- src/components/pages/Welcome.tsx
+++ src/components/pages/Welcome.tsx
@@ -6,11 +6,17 @@ import {
     TextField, 
     Button,
     Autocomplete,
-    Stack
+    Stack, 
+    ButtonBase,
+    ListItemAvatar,
+    Avatar
 } from '@mui/material';
 import { languageImage } from "../data/Image";
+import ProfileIcon from '@mui/icons-material/AccountCircle';
 import "../style/Board.css"
-
+import profileImg from "../asset/image/react.png";
+import { styled } from '@mui/material/styles';
+  
 // 회원가입 데이터- 받아온 정보
 interface UserAccountItems {
     studentId: number; //학번
@@ -27,7 +33,7 @@ const TestUserAccount : UserAccountItems = {
     password:"qwe123",
     track1: "웹공학 트랙",
     track2: "모바일소프트웨어 트랙",
-    profileImg: "heys"
+    profileImg: profileImg //기존에 있던 파일 이용 - 상단에 임포트
 }
 
 // 추가정보로 입력할 데이터
@@ -37,12 +43,33 @@ interface UserInfoItems {
     introduce?: string; //자기소개
 }
 
+//프로필 선택 버튼
+const ImageButton = styled(ButtonBase)(({ theme }) => ({
+    width: 350,
+    height: 150,
+    [theme.breakpoints.down('sm')]: {
+      width: '100% !important', // Overrides inline-style
+      height: 150,
+    },
+    '&:hover, &.Mui-focusVisible': {
+      zIndex: 1,
+      border: '2px solid #0d47a1',
+        borderRaduis: 20,
+      '& .MuiImageBackdrop-root': {
+        opacity: 0,
+      },
+    },
+    border:'1px solid #e0e0e0',
+    borderRadius: 20
+}));
+
 const Welcome: React.FC = () => {
     //닉네임, 관심기술, 자기소개
     const [nickname, setNickname] = useState<string>();
-    const [skill, setSkill] = useState<string[]>([]);
+    const [skill, setSkill] = useState<typeof languageImage>([]);
     const [introduce, setIntroduce] = useState<string>();
 
+    //닉네임, 자기소개 핸들러
     const onNicknameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setNickname(event.target.value);
         console.log(nickname);
@@ -51,6 +78,7 @@ const Welcome: React.FC = () => {
     const onIntroduceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setIntroduce(event.target.value);
         console.log(introduce);
+        console.log(skill)
     }
 
     return (    
@@ -60,107 +88,138 @@ const Welcome: React.FC = () => {
             <Typography variant="h5" align="center">추가 정보를 입력해주세요</Typography>
             <Typography variant="subtitle1" align="right">* 필수항목은 꼭 입력해주세요!</Typography>
         </Box>
-        <Box sx={{pl:15, pr:15}}>
+        <Box sx={{pl:15, pr:15, mb:5}}>
             <Stack spacing={5}>
+            <Box>
+                <Typography>* 프로필을 선택해주세요</Typography>
+                <Box sx={{
+                display:"flex",
+                justifyContent: "space-evenly",
+                mt:2
+                }}>
                 <Box>
-                    <Typography>* 프로필을 선택해주세요</Typography>
-                    <Box sx={{
-                    display:"flex",
-                    justifyContent: "space-between",
-                    }}>
-                    </Box>
+                <ImageButton>
+                    <ListItemAvatar>
+                        <Avatar alt="Travis Howard" src={TestUserAccount.profileImg} />
+                    </ListItemAvatar>
+                    <Typography
+                    variant="subtitle1"
+                    sx={{
+                        p:4
+                    }}
+                    >{TestUserAccount.name}</Typography>
+                </ImageButton>
                 </Box>
                 <Box>
-                    <Typography>* 이름</Typography>
-                    <TextField 
+                <ImageButton>
+                    <ProfileIcon sx={{fontSize:50}}/>
+                    <Typography
+                    variant="subtitle1"
+                    sx={{
+                        p:4
+                    }}
+                    >{nickname}</Typography>
+                </ImageButton>
+                </Box>
+                </Box>
+            </Box>
+            <Box>
+                <Typography>* 이름</Typography>
+                <TextField 
                     disabled
                     variant="outlined"
                     value={TestUserAccount.name}
                     fullWidth
-                    InputProps={{ sx: {  backgroundColor:"#e0e0e0" }}}
-                    />
-                </Box>
-                <Box>
-                    <Typography>* 학번</Typography>
-                    <TextField 
+                    InputProps={{ sx: { backgroundColor:"#e0e0e0" }}}
+                    sx={{mt:2}}
+                />
+            </Box>
+            <Box>
+                <Typography>* 학번</Typography>
+                <TextField 
                     disabled
                     fullWidth
                     variant="outlined"
                     value={TestUserAccount.studentId}
-                    InputProps={{ sx: {  backgroundColor:"#e0e0e0" }}}
-                    />
+                    InputProps={{ sx: { bgcolor:"#e0e0e0" }}}
+                    sx={{mt:2}}
+                />
+            </Box>
+            <Box sx={{
+                display:"flex",
+                justifyContent: "space-between",
+            }}>
+                <Box sx={{width:"400px"}}>
+                <Typography>* 1트랙</Typography>
+                <TextField 
+                    disabled
+                    fullWidth
+                    variant="outlined"
+                    value={TestUserAccount.track1}
+                    InputProps={{ sx: { backgroundColor:"#e0e0e0" }}}
+                    sx={{mt:2}}
+                />
                 </Box>
-                <Box sx={{
-                    display:"flex",
-                    justifyContent: "space-between",
-                }}>
-                    <Box sx={{width:"300px"}}>
-                        <Typography>* 1트랙</Typography>
-                        <TextField 
-                        disabled
-                        fullWidth
-                        variant="outlined"
-                        value={TestUserAccount.track1}
-                        InputProps={{ sx: {  backgroundColor:"#e0e0e0" }}}
-                        />
-                    </Box>
-                    <Box sx={{width:"300px"}}>
-                        <Typography>* 2트랙</Typography>
-                        <TextField 
-                        disabled
-                        fullWidth
-                        variant="outlined"
-                        value={TestUserAccount.track2}
-                        InputProps={{ sx: { backgroundColor:"#e0e0e0" }}}
-                        />
-                    </Box>
+                <Box sx={{width:"400px"}}>
+                <Typography>* 2트랙</Typography>
+                <TextField 
+                    disabled
+                    fullWidth
+                    variant="outlined"
+                    value={TestUserAccount.track2}
+                    InputProps={{ sx: { backgroundColor:"#e0e0e0" }}}
+                    sx={{mt:2}}
+                />
                 </Box>
-                <Box>
-                    <Typography>* 닉네임</Typography>
-                    <TextField 
+            </Box>
+            <Box>
+                <Typography>* 닉네임</Typography>
+                <TextField 
                     fullWidth
                     variant="outlined"
                     placeholder="닉네임을 입력해주세요!"
                     onChange={onNicknameChange}
-                    />
-                </Box>
-                <Box>
-                    <Typography>관심기술</Typography>
-                    <Autocomplete
-                        multiple
-                        options={languageImage}
-                        isOptionEqualToValue={(option, value) => option === value}
-                        getOptionLabel={(option) => option.name || ""}
-                        filterSelectedOptions
-                        renderInput={(params) => (
+                    sx={{mt:2}}
+                />
+            </Box>
+            <Box>
+                <Typography>관심기술</Typography>
+                <Autocomplete
+                    multiple
+                    options={languageImage}
+                    isOptionEqualToValue={(option, value) => option === value}
+                    getOptionLabel={(option) => option.name || ""}
+                    filterSelectedOptions
+                    renderInput={(params) => (
                         <TextField
                             {...params}
                             placeholder="관심 기술을 선택해주세요."
                         />
-                        )}
-                        onChange={(event, value) => {
-                            const addSkill = value as unknown as string
-                            setSkill([...skill, addSkill]);
-                          }}
-                    />
-                </Box>
-                <Box>
-                    <Typography>자기소개</Typography>
-                    <TextField 
+                    )}
+                    onChange={(event, value) => {
+                        const addSkill = value 
+                        setSkill(addSkill);
+                        console.log(skill);
+                    }}
+                    sx={{mt:2}}
+                />
+            </Box>
+            <Box>
+                <Typography>자기소개</Typography>
+                <TextField 
                     multiline
                     fullWidth
                     variant="outlined"
                     placeholder="본인소개를 해주세요."
                     rows={3}
                     onChange={onIntroduceChange}
-                    />
-                </Box>
-                <Box sx={{ display:"flex", justifyContent:"flex-end"}}>
+                    sx={{mt:2}}
+                />
+            </Box>
+            <Box sx={{ display:"flex", justifyContent:"flex-end"}}>
                 <Button sx={{mr:1}}>뒤로</Button>
-                <Button variant="contained">
-                    완료
-                </Button>
-                </Box>
+                <Button variant="contained">완료</Button>
+            </Box>
             </Stack>
         </Box>
         </Container>
