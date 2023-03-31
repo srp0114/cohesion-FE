8d5a1e55b5f348fd1a0cf684a8f3e196395fda2f
srp0114 <ksy010114@gmail.com>
2023-04-01 03:29:11 +0900

    FEAT : #22 - 로그인 토큰 없는 경우 게시판 클릭 시 모달창 출력 구현 - 로그인 토큰 없는 상태에서 블러된 게시판 클릭하는 경우 모달창 출력 - 모달창내 버튼 클릭 시, redirect 페이지로 이동 - 구분위해서 왼쪽만 블러 처리 변경 - 한성대학교 로고 이미지 추가

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -5,10 +5,30 @@ import LeftSidebar from "../layout/LeftSidebar";
 import RightSidebar from "../layout/RightSidebar";
 import Board from "../layout/Board";
 import Board2 from "../layout/Board2";
-import { Fab, Box } from "@mui/material";
+import { Fab, Box, Modal, Typography, ButtonBase, Chip } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import { useNavigate } from "react-router-dom";
 import { generateCodeChallenge, generateCodeVerifier } from "../pkce/pkce";
+import { styled } from "@mui/material/styles";
+import hansung from  "../asset/image/hansung.png";
+
+const style = {
+  borderRadius: 5,
+  p: 4,
+  bgcolor: 'background.paper',
+};
+
+//선택 버튼
+const StartButton = styled(ButtonBase)(({ theme }) => ({
+  width:600,
+  height: 80,
+  "&:hover, &.Mui-focusVisible": {
+    zIndex: 2,
+    backgroundColor: '#f7f7f7',
+    transform: 'translateY(-7%)',
+  },
+  borderRadius: 20,
+}));
 
 const WritingButton = () => {
   const navigate = useNavigate();
@@ -34,6 +54,9 @@ const Home: React.FC = () => {
 
   const [isLogin, setIsLogin] = useState<boolean>(false);
 
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => setOpen(false);
+
   //sessionStorage로부터 저장된 토큰 있는지 처음 렌더링할때만 확인
   //토큰있으면 - 게시판 보이도록
   //토큰없으면 - 게시판 블러 처리
@@ -55,22 +78,44 @@ const Home: React.FC = () => {
     navigate(`/redirect`);
   };
   
+  const openModal = () => {
+    if(!isLogin) { 
+      setOpen(!open);
+    }
+  };
+
   return (
     <>
       <button onClick={handleLogin}>로그인</button>
+
       <Grid container spacing={2}>
         <Grid xs>
           <LeftSidebar />
         </Grid>
         <Grid xs={9}>
           <Banner />
-          <Grid container spacing={2} 
-            sx={{
-              //로그인 여부에 따라 블러 처리              
-              filter: isLogin? null : "blur(2px)"
-            }}
-          >
-            <Grid xs>
+          <Grid container spacing={2}>
+            <Grid xs
+              sx={{
+                //로그인 여부에 따라 블러 처리              
+                filter: isLogin? null : "blur(1.5px)"
+              }}
+              //로그인 토큰 없는 상태에서 클릭하는 경우 - 모달창 open
+              onClick={openModal}
+            > 
+              <Modal
+                open={open}
+                onClose={handleClose}
+                sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}
+              > 
+                <Box sx={style}>
+                  <Typography align="center" variant="h5" sx={{mt:2}}>Cohesion에 오신 것을 환영합니다!</Typography>
+                  <Typography variant="subtitle1" sx={{mt:4, mb:3}} >한성대학교 로그인 페이지로 이동합니다</Typography>
+                  <StartButton onClick={() => {navigate('/redirect')}}>
+                  <img src={hansung} width="30" style={{marginRight:10}}/>
+                  한성대학교로 시작하기</StartButton>
+                </Box>
+              </Modal>
               <Board />
               <Board2 />
             </Grid>
