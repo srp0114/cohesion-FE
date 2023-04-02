f7a108dee0b9f19d708fa2839266f6a9cfdf26c9
Sauvi9non <xxxdmstj@gmail.com>
2023-04-02 19:21:43 +0900

    REFACTOR: #38 특정페이지에서 hedaer, footer 보이지 않게 처리
    -특정페이지는 추가정보페이지, 리다이렉션 페이지입니다.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,13 +2,22 @@ import React from "react";
 import Navbar from "../layout/Navbar";
 
 const Header: React.FC = () => {
+  let currentPage: string = window.location.href;
+
+  if (
+    currentPage.includes("welcome") ||
+    currentPage.includes("redirect") ||
+    currentPage.includes("authorized")
+  )
+    return null;
+  else
     return (
-        <>
+      <>
         <div>
-            <Navbar/>
+          <Navbar />
         </div>
-        </>
+      </>
     );
-  }
-  
-  export default Header;
+};
+
+export default Header;
