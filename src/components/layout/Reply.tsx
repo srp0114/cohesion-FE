5c491d4b41e5648cd7ae546a24b18910c9647ee6
srp0114 <ksy010114@gmail.com>
2023-04-03 00:36:54 +0900

    FEAT : #41 - 자유게시판 댓글 컴포넌트 추가, 작성 버튼 추가 - 댓글 작성 버튼 추가 - 자유게시판 댓글 컴포넌트 추가 - Q&A 게시판 댓글 작성 컴포넌트 분리

--- src/components/layout/Reply.tsx
+++ src/components/layout/Reply.tsx
@@ -1,46 +1,11 @@
-import React, { useState,useEffect } from "react";
-import { 
-  Typography,
-  Box
-} from "@mui/material";
-import Profile from '@mui/icons-material/AccountCircle';
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-
-// interface ReplyItems {
-//   postingID: number //게시글 id
-//   id: number; //댓글 번호
-//   writer: string;  //작성자
-//   content: string; //댓글 내용
-//   createdDate: string; //작성일
-//   modifiedDate?: string; //수정일
-//   parentID? : number; //부모 댓글 id
-//   isChosen?: boolean; //선택여부 - Q&A, 모집게시판에서 사용
-//   reports: number; //신고
-// }
-
-//axios get 이전에 UI 확인을 위한 댓글 데이터
-// const TestReply : ReplyItems[] = [ 
-//   {
-//   postingID: 2,
-//   id: 1,
-//   writer: "young",
-//   content: "컴포넌트 매개변수 전달은 props를 이용하시면 됩니다.",
-//   createdDate: "1초전",
-//   reports: 0
-//   },
-//   {
-//     postingID: 2,
-//     id: 2,
-//     writer: "서영",
-//     content: "defaultProps로 값을 설정하면 전달할 수 있어요!",
-//     createdDate: "1시간전",
-//     reports: 0
-//   },
-// ]
+import {  Typography, Box, TextField, Button } from "@mui/material";
+import Profile from '@mui/icons-material/AccountCircle';
 
 interface User {
   id : number;
-  nickname : String;
+  nickname : string;
 }
 
 interface ReplyItems{
@@ -55,51 +20,75 @@ interface ReplyItems{
 }
 
 interface ReplyProps{
-  id : String | undefined;
+  postingID?: string;
+}
+  
+// 댓글 필드 및 버튼 컴포넌트 
+// 댓글 작성 버튼 추가
+const ReplyField = () => {
+
+  // 댓글 게시 버튼 클릭 시 적용될 핸들러
+  const SubmitHandler = () => {
+    useEffect(() => {
+      // axios - post 여기에 작업해주시면 됩니다!
+      
+    })
+  }
+
+  return (
+    <>
+      <Box>
+      <TextField
+        fullWidth
+        placeholder="댓글을 입력하세요."
+        variant="outlined"
+        multiline
+        sx={{ mt: 2, mb: 2}}
+      />
+      <Box display="flex" justifyContent="flex-end">
+      <Button onClick={SubmitHandler} size="large">작성하기</Button>
+      </Box>
+    </Box>
+    </>
+  )
 }
 
-
-
-//상위 컴포넌트로 부터 게시글 id 값 받아오기 
-const Reply: React.FC<ReplyProps> = ({id}) => {
+// 상위 컴포넌트로 부터 게시글 id 값 받아오기 
+// 기존 id 변수명 postingID로 변경
+const Reply: React.FC<ReplyProps> = ({postingID}) => {
 
   const[replyData ,setReplyData] = useState<ReplyItems[]>([]);
-  /*
-  초기값 없는 경우(undefiend) -> 댓글 없습니다 출력
-  */
 
   useEffect(()=>{
       axios
-      .get("/api/qnaBoards/"+id+"/replies")
+      .get("/api/qnaBoards/"+postingID+"/replies")
       .then((res)=>setReplyData(res.data));
   },[])
 
-  // 대댓글이 들어갈 컨테이너 (테스트용으로 임시로 해놨습니다. 바꿔주시면 됩니다!)
-  const replyContainer = (replies: ReplyItems[], parentId?: number) => {
-    const filteredReplies = parentId ? replies.filter((reply) => reply.parentId === parentId) : replies;
+// 대댓글이 들어갈 컨테이너 (테스트용으로 임시로 해놨습니다. 바꿔주시면 됩니다!)
+const replyContainer = (replies: ReplyItems[], parentId?: number) => {
+  const filteredReplies = parentId ? replies.filter((reply) => reply.parentId === parentId) : replies;
 
-    return filteredReplies.length > 0 && (
-      <Box sx={{ ml: 6 }}>
-        {filteredReplies.map((reply) => (
-          <div key={reply.id}>
-            <Box sx={{ display: 'flex', mt: 5 }}>
-              <Profile fontSize="large" />
-              <Box sx={{ mt: 0.3 }}>
-                <Typography variant="h6" sx={{ ml: 1 }}>{reply.user.nickname}</Typography>
-                <Typography variant="subtitle2" sx={{ ml: 1 }}> {reply.createdAt}</Typography>
-              </Box>
+  return filteredReplies.length > 0 && (
+    <Box sx={{ ml: 6 }}>
+      {filteredReplies.map((reply) => (
+        <div key={reply.id}>
+          <Box sx={{ display: 'flex', mt: 5 }}>
+            <Profile fontSize="large" />
+            <Box sx={{ mt: 0.3 }}>
+              <Typography variant="h6" sx={{ ml: 1 }}>{reply.user.nickname}</Typography>
+              <Typography variant="subtitle2" sx={{ ml: 1 }}> {reply.createdAt}</Typography>
             </Box>
-            <Box>
-              <Typography sx={{ ml: 5, mt: 1, mb: 5 }}>{reply.article}</Typography>
-            </Box>
-            {replyContainer(replies, reply.id)}
-          </div>
-        ))}
-      </Box>
-    );
-  };
-
-
+          </Box>
+          <Box>
+            <Typography sx={{ ml: 5, mt: 1, mb: 5 }}>{reply.article}</Typography>
+          </Box>
+          {replyContainer(replies, reply.id)}
+        </div>
+      ))}
+    </Box>
+  );
+};
 
   const reply = replyData.length ? (
     replyData.map((value) => {
@@ -119,14 +108,16 @@ const Reply: React.FC<ReplyProps> = ({id}) => {
             <Typography sx={{ml: 5, mt: 1, mb: 5}}>{value.article}</Typography>
           </Box>
           {replyContainer(replyData,value.id)}
+
         </div>
       )
     })
     
-  ) : (<Typography variant="h5" sx={{color:"grey", m:2}}>아직 댓글이 없습니다.</Typography>)
+  ) : (<Typography variant="h6" sx={{color:"grey", m:2}}>아직 댓글이 없습니다.</Typography>)
 
   return (
     <>
+      <ReplyField/>
       {reply}
     </>
   );
