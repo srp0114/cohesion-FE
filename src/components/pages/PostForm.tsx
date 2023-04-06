922665666ff4aac046d5a2d4390c8063934b1bc5
Sauvi9non <xxxdmstj@gmail.com>
2023-04-06 13:45:01 +0900

    FEAT: #39 구인게시판 작성폼 추가
    -People 리팩토링 필요

--- src/components/pages/PostForm.tsx
+++ src/components/pages/PostForm.tsx
@@ -9,11 +9,17 @@ import {
   Select,
   MenuItem,
   Menu,
+  Stack,
+  Typography,
+  Box,
 } from "@mui/material";
 import axios from "axios";
 import Point from "../layout/Point";
 import Skill from "../layout/Skill";
 import EditorToolbar from "../layout/EditorToolbar";
+import People from "../layout/People";
+import { ConditionRequired, ConditionOptional } from "../layout/Condition";
+
 /*
  * 기본 게시글 작성 UI폼
  */
@@ -23,6 +29,8 @@ const PostForm = () => {
   const [content, setContent] = useState("");
   const [point, setPoint] = useState<number>(0);
   const [skill, setSkill] = useState<string>("");
+  const [required, setRequired] = useState<string>("");
+  const [optional, setOptional] = useState<string>("");
 
   //내용, 포인트 , 언어 컴포넌트로부터 데이터 받아오기
   const getContent = (value: string) => {
@@ -36,6 +44,14 @@ const PostForm = () => {
   const getSkill = (value: string) => {
     setSkill(value);
     console.log(value);
+  };
+
+  const getRequired = (value: string) => {
+    setRequired(value);
+  };
+
+  const getOptional = (value: string) => {
+    setOptional(value);
   };
 
   const boardHandler = (event: SelectChangeEvent<unknown>) => {
@@ -115,6 +131,8 @@ const PostForm = () => {
       } catch (err) {
         console.log("CreateBoard/handleInput/err: ", err);
       }
+    } else if (boardType === "recruit") {
+      // 구인 게시판인 경우
     }
   };
 
@@ -124,82 +142,69 @@ const PostForm = () => {
   const SelectPoint =
     boardType === "question" ? <Point getPoint={getPoint} /> : null;
 
+  const DesignateConditionRequired =
+    boardType === "recruit" ? <ConditionRequired getRequired={getRequired}/> : null;
+  const DesignateConditionOptional =
+    boardType === "recruit" ? <ConditionOptional getOptional={getOptional}/> : null;
+  const DesignatePeople = boardType === "recruit" ? <People /> : null;
+
   return (
     <>
       <Container>
         <Grid container direction="column" spacing={2}>
-          <Grid item>
-            <FormControl style={{ minWidth: "120px" }}>
-              <Select value={boardType} onChange={boardHandler} size="small">
-                <MenuItem value={"free"} defaultChecked>
-                  자유게시판
-                </MenuItem>
-                <MenuItem value={"question"}>Q&A게시판</MenuItem>
-                <MenuItem value={"recruit"}>구인게시판</MenuItem>
-                <MenuItem value={"notice"}>공지사항</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-          {SelectSkill}
-          <Grid item>
-            <TextField
-              className="board title"
-              id="board_title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              maxRows={1}
-              placeholder={"제목"}
-              fullWidth
-            ></TextField>
-          </Grid>
-          <Grid item>
-            <EditorToolbar getContent={getContent} />
-            {/* value: {content} */}
-            <div>
-              <input type="file" multiple onChange={onSaveFiles} />
-            </div>
-          </Grid>
+          <>
+            <Grid item>
+              <FormControl style={{ minWidth: "120px" }}>
+                <Select value={boardType} onChange={boardHandler} size="small">
+                  <MenuItem value={"free"} defaultChecked>
+                    자유게시판
+                  </MenuItem>
+                  <MenuItem value={"question"}>Q&A게시판</MenuItem>
+                  <MenuItem value={"recruit"}>구인게시판</MenuItem>
+                  <MenuItem value={"notice"}>공지사항</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            {SelectSkill}
+            <Grid item>
+              <TextField
+                className="board title"
+                id="board_title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                maxRows={1}
+                placeholder={"제목"}
+                fullWidth
+              ></TextField>
+            </Grid>
+            <Grid item>
+              <EditorToolbar getContent={getContent} />
+              {/* value: {content} */}
+              <div>
+                <input type="file" multiple onChange={onSaveFiles} />
+              </div>
+            </Grid>
 
-          {SelectPoint}
-          <Grid item>
-            <Button
-              className="board button"
-              variant="outlined"
-              disableElevation
-              onClick={submitHandler}
-            >
-              게시
-            </Button>
-          </Grid>
+            {SelectPoint}
+
+            {DesignateConditionRequired}
+            {DesignateConditionOptional}
+            {DesignatePeople}
+
+            <Grid item>
+              <Button
+                className="board button"
+                variant="outlined"
+                disableElevation
+                onClick={submitHandler}
+              >
+                게시
+              </Button>
+            </Grid>
+          </>
         </Grid>
       </Container>
     </>
-  );
-};
-
-/*
- * 구인 게시판에서 구인 조건을 작성할 때 사용
- */
-const Condition = () => {
-  return (
-    <Grid
-      container
-      spacing={4}
-      direction="column"
-      justifyContent="space-around"
-    >
-      <Grid item>
-        <TextField
-          required
-          label="필수"
-          placeholder="필수 조건을 기입하세요."
-        ></TextField>
-        <TextField
-          label="우대 사항"
-          placeholder="우대 사항을 기입하세요."
-        ></TextField>
-      </Grid>
-    </Grid>
   );
 };
 
