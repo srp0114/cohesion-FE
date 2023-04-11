6233d39ab241fc5efad9d01c51568a20e5c1a88b
Sauvi9non <xxxdmstj@gmail.com>
2023-04-12 01:11:19 +0900

    FEAT:#55 마이페이지 기본 UI 작업 완료
    -프로필 이미지 <Image />에서 기본 html <img>로 수정

--- src/components/pages/MyPage.tsx
+++ src/components/pages/MyPage.tsx
@@ -2,48 +2,67 @@ import React from "react";
 import {
   Badge,
   Box,
+  Divider,
   Grid,
   IconButton,
   Typography,
   Stack,
   Paper,
 } from "@mui/material";
-import Image from "mui-image";
 import ChatIcon from "@mui/icons-material/ChatBubbleOutline"; //댓글
 import BookmarkIcon from "@mui/icons-material/BookmarkBorder"; //북마크
 import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined"; //게시글
 import Money from "@mui/icons-material/MonetizationOn"; //포인트
 import CreateOutlinedIcon from "@mui/icons-material/CreateOutlined"; //작성 및 수정
 import MoreHorizOutlinedIcon from "@mui/icons-material/MoreHorizOutlined"; //더보기
+import { Track } from "../model/user";
+import { Reply } from "../model/reply";
+import { Board } from "../model/board";
+
+export interface MyPageItems {
+  //논의 후 수정 必
+  studentId: string; //사용자 고유식별번호, 학번, 사용자의 아이디
+  profileImg: string; //추가정보에서 선택한 프로필사진
+  nickname: string; //추가정보페이지의 닉네임
+  track1: Track; //1트랙 1학년인 경우 1,2트랙 모두 none으로 나옴.
+  track2: Track; //2트랙
+  reply?: Array<Reply>; //자신이 작성한 댓글의 배열
+  board?: Array<Board>; //자신이 작성한 게시글의 배열
+  bookmark?: Array<Board>; //북마크한 게시글들의 배열
+  point: number; //사용자의 포인트
+  dailySummary: Array<Board>; //오늘 공부 기록
+  skill?: string; //추가정보페이지에서 선택한 관심있는 기술, 라이브러리나 프레임워크 의미
+  language?: string; //추가정보페이지에서 선택한 언어, js, java, c, python의 언어 의미
+  selfIntroduction: string; //추가정보페이지에서 입력한 자기소개
+}
 
 const MyPage: React.FC = () => {
   //추가정보페이지 입력 전까지는 들어오지 못하게 해야함.
   return (
     <>
-      <Typography variant="h3">마이페이지</Typography>
       <Grid
         container
-        columnSpacing={{ md: "36px", xs: "24px" }}
+        columnSpacing={{ md: "2.5rem", xs: "1.5rem" }} //1rem == 16px, 2.5rem == 36px, 1.5rem == 24px, 1.125rem === 18px
         rowSpacing={4}
         direction="row"
       >
-        <Grid item xs={12} md={5}>
+        <Grid item xs={12} md={4}>
           <MyProfile />
         </Grid>
 
         <Grid
           container
           item
-          rowSpacing={{ md: "36px", xs: "24px" }}
+          rowSpacing={{ md: "2.5rem", xs: "1.5rem" }}
           xs={12}
-          md={7}
+          md={8}
           direction="column"
         >
           <Grid item>
             <MyHistory />
           </Grid>
 
-          <Grid container item rowSpacing={{ md: "18px" }}>
+          <Grid container item rowSpacing={{ xs: "1.125rem" }}>
             <Grid item xs={12}>
               <MySummary />
             </Grid>
@@ -68,27 +87,44 @@ const MyProfile: React.FC = () => {
         sx={{
           border: "0.5px solid black",
           borderRadius: "20px",
-          padding: "5px",
-          margin: "5px",
+          padding: "1.125rem",
         }}
       >
-        <Typography>HANSUNG UNIVERSITY COMPUTER ENGINEERING</Typography>
-        <Image
-          src="https://pbs.twimg.com/profile_images/1212031261297930241/p6kIo01N_400x400.jpg"
-          width={250}
-          height={250}
-          duration={325}
-        />
-        <Box>
-          <Typography>닉네임</Typography>
-          <Typography>종강시켜주세요</Typography>
-        </Box>
-        <Box>
-          <Typography>소속트랙</Typography>
-          <Typography>디지털콘텐츠 가상현실</Typography>
-          <Typography>웹공학</Typography>
-        </Box>
-        <Typography>COHENSION</Typography>
+        <Grid
+          container
+          rowSpacing={{ xs: "2.5rem" }}
+          direction="column"
+          sx={{ padding: "0 3rem", textAlign: "center" }}
+        >
+          <Grid item>
+            <Typography>HANSUNG UNIVERSITY</Typography>
+            <Typography>COMPUTER ENGINEERING</Typography>
+          </Grid>
+          <Grid item>
+            <img
+              src="https://pbs.twimg.com/profile_images/1212031261297930241/p6kIo01N_400x400.jpg"
+              width="250px"
+              height="250px"
+              style={{ border: "1px solid black" }}
+            />
+          </Grid>
+          <Grid item>
+            <Box>
+              <Typography variant="body1">닉네임</Typography>
+              <Typography variant="h6">종강시켜주세요</Typography>
+            </Box>
+          </Grid>
+          <Grid item>
+            <Box>
+              <Typography variant="body2">소속트랙</Typography>
+              <Typography variant="h6">디지털콘텐츠 가상현실</Typography>
+              <Typography variant="h6">웹공학</Typography>
+            </Box>
+          </Grid>
+          <Grid item>
+            <Typography>COHENSION</Typography>
+          </Grid>
+        </Grid>
       </Paper>
     </>
   );
@@ -131,13 +167,13 @@ const MyHistory: React.FC = () => {
             </IconButton>
           </Box>
         </Stack>
-
-        <Box>
+        <Divider orientation="vertical" variant="middle" flexItem />
+        <Stack direction="row">
           <Typography>누적포인트</Typography>
           <IconButton disabled color="primary" size="large">
-            <Money /> <Typography>1080</Typography>
+            <Money /> <Typography sx={{ fontSize: "2rem" }}>1080</Typography>
           </IconButton>
-        </Box>
+        </Stack>
       </Stack>
     </>
   );
@@ -153,27 +189,40 @@ const MySummary: React.FC = () => {
       sx={{
         border: "0.5px solid black",
         borderRadius: "20px",
-        padding: "5px",
-        margin: "5px",
+        padding: "1.125rem", //18px
       }}
     >
-      <Box>
-        <Typography>오늘의 공부 기록 요약</Typography>
+      <Stack
+        direction="row"
+        sx={{ display: "flex", justifyContent: "space-between" }}
+      >
+        <Typography>오늘의 공부 기록 요약(가제)</Typography>
+
         <IconButton>
           <CreateOutlinedIcon />
         </IconButton>
-      </Box>
-      <Box>
-        <Typography>
-          오늘 공부한 내용에 대해 짤막한 요약글을 작성한다.
-        </Typography>
-      </Box>
-      <Box>
+      </Stack>
+
+      <Typography>
+        Decorators are an upcoming ECMAScript feature that allow us to customize
+        classes and their members in a reusable way. 오늘 공부 기록에 대한
+        문장입니다.
+      </Typography>
+
+      <Stack
+        direction="row"
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginTop: "1.125rem",
+        }}
+      >
         <Typography>작성날짜. 수정날짜는 별도로 표시X?</Typography>
+
         <IconButton>
           <MoreHorizOutlinedIcon />
         </IconButton>
-      </Box>
+      </Stack>
     </Paper>
   );
 };
@@ -188,24 +237,33 @@ const MyIntroduction: React.FC = () => {
       sx={{
         border: "0.5px solid black",
         borderRadius: "20px",
-        padding: "5px",
-        margin: "5px",
+        padding: "1.125rem",
       }}
     >
-      <Box>
+      <Stack
+        direction="row"
+        sx={{ display: "flex", justifyContent: "space-between" }}
+      >
         <Typography>유저닉네임의 자기 소개</Typography>
         <IconButton>
           <CreateOutlinedIcon />
         </IconButton>
-      </Box>
+      </Stack>
 
-      <Box>
-        <Typography>추가정보에서 작성한 기술스택</Typography>
-      </Box>
+      <Grid container rowSpacing={"1.125rem"} direction="column">
+        <Grid item>
+          <Typography>추가정보에서 작성한 기술스택</Typography>
+          <Typography>리액트, C, Javascript</Typography>
+        </Grid>
 
-      <Box>
-        <Typography>추가정보에서 작성한 자기소개</Typography>
-      </Box>
+        <Grid item>
+          <Typography>추가정보에서 작성한 자기소개</Typography>
+
+          <Typography>
+            내가 뭘 할 수 있는지에 대해서 작성을 해놨습니다.
+          </Typography>
+        </Grid>
+      </Grid>
     </Paper>
   );
 };
