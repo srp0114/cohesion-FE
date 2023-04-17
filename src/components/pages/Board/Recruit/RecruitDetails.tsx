2346110972ce1af23822c108d5d1e2a2d47ee2ee
Sauvi9non <xxxdmstj@gmail.com>
2023-04-17 14:46:54 +0900

    STYLE:#76 상세보기 UI 기본 통합 완료

--- src/components/pages/Board/Recruit/RecruitDetails.tsx
+++ src/components/pages/Board/Recruit/RecruitDetails.tsx
@@ -1,11 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Time from "../../../layout/Time";
-import { Avatar, Box, Stack, Typography, IconButton } from "@mui/material";
+import { Avatar, Box, Grid, Stack, Typography, IconButton } from "@mui/material";
 import BookmarkIcon from "@mui/icons-material/BookmarkBorder";
+import Visibility from "@mui/icons-material/VisibilityOutlined";
 import Person2OutlinedIcon from "@mui/icons-material/Person2Outlined";
 import { data } from "../../../data/RecruitData";
 import axios from "axios";
+import Reply from "../../../layout/Reply/Reply";
+import { PostingCrumbs } from "../../../layout/postingDetail/postingCrumbs";
+import { replyCount } from "../../../layout/postingDetail/replyCount";
+import { bookmarkNviews } from "../../../layout/postingDetail/bookmarkNviews";
+import { userInfo } from "../../../layout/postingDetail/userInfo";
+import { PageName } from "../../../layout/postingDetail/postingCrumbs";
+import Loading from "../../../layout/Loading";
 
 //모집 상세보기 인터페이스
 export interface RecruitDetailItems {
@@ -48,86 +56,98 @@ const RecruitDetails: React.FC = (): JSX.Element => {
 
   const detailPosting = postItem ? (
     <>
-      <Box sx={{ paddingLeft: 3, paddingRight: 3 }}>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-          }}
-        >
-          <Typography variant="h5" sx={{ marginBottom: 1, fontWeight: 600 }}>
+      <Grid container direction="column" rowSpacing={"3rem"}>
+        {/*게시판 이름, BreadCrumbs */}
+        <Grid item xs={12}>
+          <PostingCrumbs title={postItem.title} board="recruit" />
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h4" gutterBottom>
             {postItem.title}
           </Typography>
-          <Typography variant="caption">
-            <Time date={postItem.createdDate} />
+        </Grid>
+        {/*작성자 정보 , 작성 시각 */}
+        <Grid item container xs={12} justifyContent={"space-between"}>
+          <Grid item xs={4}>
+            {userInfo(postItem.writer, postItem.profileImg, postItem.stuId)}
+          </Grid>
+
+          <Grid item justifyContent={"flex-end"}>
+            <Time date={postItem.createdDate} />{" "}
+            {/*은서: Time 컴포넌트 Typography 수정 가능하도록 수정 필요*/}
+          </Grid>
+        </Grid>
+
+        {/*게시글 내용 */}
+        <Grid item xs={12} sx={{ padding: "0 2.5rem" }}>
+          <Typography variant="h5">
+            <div dangerouslySetInnerHTML={{ __html: postItem.content }} />
+            {/* 이미지에 대해서는 추후 논의 후 추가)*/}
           </Typography>
-        </Box>
+        </Grid>
 
-        <Box sx={{ marginBottom: 5 }}>
-          <Stack direction="row">
-            <Avatar
-              srcSet={postItem.profileImg as string}
-              sx={{ width: "30px", height: "30px", marginRight: "5px" }}
-            />
-            <Typography variant="body2">
-              {`${postItem.writer} (${postItem.stuId.toString().slice(0,2)}학번)`}
+        <Grid item container xs={12} direction="row" columnSpacing={"3rem"}>
+          {!!postItem.optional ? <><Grid item xs={6} md={12}>
+            <Typography sx={{fontSize:"1.75rem"}}>필수</Typography>
+            <Typography variant="h5">
+              <div dangerouslySetInnerHTML={{ __html: postItem.require }} />
             </Typography>
-          </Stack>
-        </Box>
+          </Grid>
+          <Grid item xs={6} md={12}>
+            <Typography sx={{fontSize:"1.75rem"}}>우대</Typography>
+            <Typography variant="h5">
+              <div dangerouslySetInnerHTML={{ __html: postItem.optional }} />
+            </Typography>
+          </Grid></> : <Grid item xs={12}>
+            <Typography sx={{fontSize:"1.75rem"}}>필수</Typography>
+            <Typography variant="h5">
+              <div dangerouslySetInnerHTML={{ __html: postItem.require }} />
+            </Typography>
+          </Grid>}
+        </Grid>
 
-        <Box sx={{ marginBottom: 1 }}>
-          <div dangerouslySetInnerHTML={{ __html: postItem.require }} />
-          {/* 이미지에 대해서는 추후 논의 후 추가)*/}
-          {!postItem.optional ? (
-            <></>
-          ) : (
-            <div dangerouslySetInnerHTML={{ __html: postItem.optional }} />
-          )}
-        </Box>
-        <Box borderRadius={7}>
-          <Typography variant="h4">
+        <Grid item xs={12} sm={6}>
+          <Typography sx={{ fontSize: "0.75" }}>
             {postItem.gathered} / {postItem.party}
           </Typography>
-        </Box>
-        <Box sx={{ marginTop: 3, marginBottom: 3 }}>
-          <Stack direction="row" sx={{ disply: "flex", justifyContent: "end" }}>
-            <IconButton size="small">
-              <Person2OutlinedIcon /> {postItem.views}
-            </IconButton>
-            <IconButton size="small">
-              <BookmarkIcon /> {postItem.bookmark}
-            </IconButton>
-          </Stack>
-        </Box>
-        <Box>
-          <Typography
-            variant="body1"
-            sx={{ marginBottom: 5, paddingLeft: 3, fontWeight: 600 }}
+        </Grid>
+
+        {/*북마크, 조회수 이 컴포넌트 따로 빼둘것  */}
+        <Grid item xs={12} sm={6}>
+          <Stack
+            direction="row"
+            spacing={"0.75rem"}
+            sx={{ display: "flex", justifyContent: "flex-end" }}
           >
-            {`${postItem.reply}개의 댓글이 있습니다.`}
-          </Typography>
-          <Box sx={{ border: "1px solid #787878", height: "50%" }}>
-            {/*은서: 추후 이곳에 댓글 목록, 댓글 작성란 등 컴포넌트 추가하기*/}
-          </Box>
-        </Box>
-      </Box>
+            <Box>
+              <IconButton size="small" disabled>
+                <Visibility fontSize="large" />
+                <Typography variant="h5">{postItem.views}</Typography>
+              </IconButton>
+            </Box>
+
+            <Box>
+              <IconButton size="small">
+                <BookmarkIcon fontSize="large" />
+                <Typography variant="h5">{postItem.bookmark}</Typography>
+              </IconButton>
+            </Box>
+          </Stack>
+          {/*bookmarkNviews(postItem.bookmark, onClickBookmark, bookmarkCount) 북마크 기능 추가 시 여기 주석만 지워주시면 됩니다. 은서*/}
+        </Grid>
+
+        {/*댓글 */}
+        {replyCount(postItem.reply)}
+      </Grid>
+
+      <Reply postingID={id} />
     </>
   ) : (
-    <Typography>no data</Typography>
+    <Loading />
   );
 
   return (
-    <>
-      <Box
-        sx={{
-          borderLeft: "1px solid black",
-          borderRight: "1px solid black",
-          padding: 10,
-        }}
-      >
-        {detailPosting}
-      </Box>
-    </>
+    <Box sx={{ padding: "2.25rem 10rem 4.5rem" }}>{detailPosting}</Box>
   );
 };
 
