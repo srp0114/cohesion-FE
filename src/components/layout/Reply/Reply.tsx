ab2a012a45f2b6b21d97d311b91758f3bf834fea
srp0114 <ksy010114@gmail.com>
2023-04-21 03:00:24 +0900

    FEAT : #94 - 댓글 수정창 추가

--- src/components/layout/Reply/Reply.tsx
+++ src/components/layout/Reply/Reply.tsx
@@ -6,7 +6,7 @@ import NestedReplyField from "./NestedReplyField";
 import PinReply from "./PinReply";
 import Time from "../Time";
 import Profile from "@mui/icons-material/AccountCircle";
-
+import EditReply from "./EditReply";
 interface User {
   id: number;
   nickname: string;
@@ -32,6 +32,10 @@ const Reply = ( props : ReplyProps) => {
   const [replyData, setReplyData] = useState<ReplyItems[]>([]);
   const [userId,setUserId] = useState<number>(0);
   const [isChosen, setIsChosen] = useState<boolean>(false);
+
+  // 댓글 수정 id, 여부를 위한 상태변수 추가
+  const [editReplyId, setReplyId] = useState<number>(0);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
 
   // 기존 FreeReply, QnAReply 삭제 후, Reply로 통일 (api 주소, 작성창, 체크박스 외 동일해서)
   // Details 컴포넌트에서 Reply 컴포넌트 호출 시 해당 게시판, 게시판 번호 전달
@@ -115,9 +119,30 @@ const Reply = ( props : ReplyProps) => {
       });
   };
 
-  const editReply = () => {
-    // 변경 api 추가
-      //수정 창 생기고 진행
+  // 수정 버튼 클릭한 경우 - 기존 댓글 내용이 수정창으로 변경
+  // 댓글 번호, 내용, 부모댓글 번호 (답글인 경우) 매개변수로 받아옴
+  // TODO : url 변경 api 작업
+  const editReply = (id: number, article: string, parentId?: number) => {
+    setIsEditing(false);
+
+    const data = {
+      article: article,
+      parentId: parentId,
+    };
+
+    // 변경 api 
+    axios({
+      method: "put",
+      url: ``,
+      headers: { "Content-Type": "application/json" },
+      data: JSON.stringify(data),
+    }).then((res) => {
+      // 변경된 경우
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
   };
 
   const deleteReply = (replyId : number) => {
@@ -146,21 +171,38 @@ const Reply = ( props : ReplyProps) => {
   // 게시글 작성 시에도 현재 사용자의 id 필요 
   // 현재는 모든 사용자 체크박스 확인 가능
   // TODO : && userId === props.writerId 인 경우에도 버튼 출력하도록 조건 추가
-  const ChooseReply = (article: string) => { 
-    return board === "qna" ? (
-      <>
-      <Grid container spacing={2}>
-          <Grid item xs={11}>
-            <div className="ql-snow">                
-              <div className="ql-editor" dangerouslySetInnerHTML={{ __html: article }} />
-            </div>
-          </Grid>
-          <Grid item>
-            <PinReply onReplyCheck={handleChooseReply} isChosen={isChosen}/>
-          </Grid>
-      </Grid>
+  const Article = (article: string) => { 
+    return ( 
+      <> { board === "qna" ? 
+        // Q&A 게시판인 경우
+        <Grid container spacing={2}>
+            <Grid item xs={11}>
+              <div className="ql-snow">                
+                <div className="ql-editor" dangerouslySetInnerHTML={{ __html: article }} />
+              </div>
+            </Grid>
+            <Grid item>
+              <PinReply onReplyCheck={handleChooseReply} isChosen={isChosen}/>
+            </Grid>
+        </Grid>
+         :  
+        // 그 외 나머지 게시판이 경우
+        <Typography>{article}</Typography>
+      }
       </>
-    ) : ( <Typography>{article}</Typography>);
+    )
+  }
+
+  // 수정 버튼 클릭에 따른 컴포넌트 전환
+  const Edit = (article:string, id: number, parentId?:number) => {
+    // 수정버튼 클릭한 댓글 번호, 수정중인 경우 - 수정 컴포넌트로 전환
+    return editReplyId === id && isEditing? (
+      <EditReply id={id} article={article} parentId={parentId} isEditing={isEditing} onChangeReply={editReply} />
+    ) : (
+      <>
+        {Article(article)}
+      </>
+    )
   }
 
   //사용자 확인은 해당 접속 유저의 id를 받아온 상태에서 map 함수 안에서 확인 하였습니다.
@@ -198,17 +240,23 @@ const Reply = ( props : ReplyProps) => {
                   </Box>
                 </Box>
                 <Box>{reply.user.id === userId ?  <>
-                    <Button onClick={editReply}>수정</Button>
+                    <Button onClick={() => { 
+                      setReplyId(reply.id); 
+                      setIsEditing(true)}}>수정</Button>
                     <Button onClick={()=>deleteReply(reply.id)}>삭제</Button>
-                </> : null}</Box>
+                </> : null}
+                </Box>
               </Box>
               <Box>
-                <Typography sx={{ ml: 5, mt: 1, whiteSpace: "pre-wrap"}}>{reply.article}</Typography>
+                <Typography sx={{ ml: 5, mt: 1, whiteSpace: "pre-wrap"}}>
+                  {Edit(reply.article, reply.id, reply.parentId)}
+                </Typography>
               </Box>
               <NestedReplyField
-                onAddNested={handleAddNested}
-                parentId={reply.id}
+              onAddNested={handleAddNested}
+              parentId={reply.id}
               />
+              
               {replyContainer(replies, reply.id)}
             </div>
           ))}
@@ -246,12 +294,15 @@ const Reply = ( props : ReplyProps) => {
                 </Box>
               </Box>
                 <Box>{value.user.id === userId ?  <>
-                    <Button onClick={editReply}>수정</Button>
+                    <Button onClick={() => { 
+                      setReplyId(value.id); 
+                      setIsEditing(true)}}>수정</Button>
                     <Button onClick={()=>deleteReply(value.id)}>삭제</Button>
-            </> : null}</Box>
+              </> : null}
+              </Box>
             </Box>
             <Box sx={{display:"flex", justifyContent:"space-between", ml:3, mt:2, mr: 3, whiteSpace: "pre-wrap" }}>
-              {ChooseReply(value.article)}
+              {Edit(value.article, value.id)}
             </Box>
             <NestedReplyField
               onAddNested={handleAddNested}
