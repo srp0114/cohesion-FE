d3607af49072322abb6e64fc3e7a9f219af5ebbc
Sauvi9non <xxxdmstj@gmail.com>
2023-04-24 04:47:02 +0900

    FEAT:#76 자유게시판 목록, 상세보기 Skeleton 추가

--- src/components/pages/Board/Free/FreeBoard.tsx
+++ src/components/pages/Board/Free/FreeBoard.tsx
@@ -10,7 +10,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { reply_bookmark_views } from "../../../layout/Board/reply_bookmark_views";
 import { userInfo } from "../../../layout/postingDetail/userInfo";
 import { shortenContent } from "../QnA/QnABoard";
-
+import { BoardSkeleton } from "../../../layout/Skeletons";
 
 //자유게시판 페이지 인터페이스
 export interface FreeBoardItems {
@@ -30,8 +30,10 @@ export interface FreeBoardItems {
 const FreeBoard = () => {
   const [freeData, setFreeData] = useState<FreeBoardItems[]>([]);
   const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false); //loading이 false면 skeleton, true면 게시물 목록 
 
   useEffect(() => {
+    setLoading(false); //마운트될 때, api 요청 보내기 전 skeleton
     const curPage = page - 1;
     axios({
       method: "get",
@@ -51,6 +53,10 @@ const FreeBoard = () => {
       });
   }, []);
 
+  useEffect(() => {
+    setLoading(true); //freeData 상태가 변할 때 게시글 목록
+  }, [freeData]);
+
   const displayPosting = freeData.map((element, idx) => {
     return (
       <>
@@ -61,26 +67,33 @@ const FreeBoard = () => {
 
   );
 
-  return (
-    <Box sx={{ padding: "2.25rem 10rem 4.5rem" }}>
-      <Typography
-        variant="h5" >
-        자유게시판
-      </Typography>
-      <FilterPosting />
-      {displayPosting}
-      <PaginationControl
-        page={page}
-        between={1}
-        total={100}
-        limit={20}
-        changePage={(page: React.SetStateAction<number>) => setPage(page)}
-        ellipsis={1}
-      />
-      <WritingButton />
-    </Box>
-  );
-};
+  return (<>
+    {
+      loading ? (
+        <Box sx={{ padding: "2.25rem 10rem 4.5rem" }}>
+          <Typography
+            variant="h5" >
+            자유게시판
+          </Typography>
+          <FilterPosting />
+          {displayPosting}
+          <PaginationControl
+            page={page}
+            between={1}
+            total={100}
+            limit={20}
+            changePage={(page: React.SetStateAction<number>) => setPage(page)}
+            ellipsis={1}
+          /><WritingButton /></Box>
+      )
+        : (<Box sx={{ padding: "2.25rem 10rem 4.5rem" }}>
+          <BoardSkeleton />
+          <WritingButton />
+        </Box>)
+    }
+  </>);
+
+}
 
 const PreviewPosting: React.FunctionComponent<FreeBoardItems> = (
   props: FreeBoardItems
@@ -99,43 +112,41 @@ const PreviewPosting: React.FunctionComponent<FreeBoardItems> = (
       border: "0.5px solid black",
       "&:hover": {
         boxShadow: 5,
-        pointer:"cursor"
+        pointer: "cursor"
       },
       margin: "2.25rem 0",
       padding: "0.75rem 2rem 1.25rem",
-      height:"16rem", //게시글 박스 높이
-      justifyContent:"space-between",
-      alignItems:"stretch"
+      height: "16rem", //게시글 박스 높이
+      justifyContent: "space-between",
+      alignItems: "stretch"
     }}
       onClick={() => goToPost(props.id)}>
       <Grid item sx={{ display: "flex", justifyContent: "space-between" }}>
-      <Typography variant="h5" >
+        <Typography variant="h5" >
           {props.title}
         </Typography>
-        <Time date={props.createdDate} variant="h6"/>
+        <Time date={props.createdDate} variant="h6" />
       </Grid>
 
       <Grid item sx={{
-      whiteSpace: "pre-line",
-      wordWrap: "break-word",
-      overflow: "hidden",
-      textOverflow: "ellipsis",
-      alignItems: "stretch",
+        whiteSpace: "pre-line",
+        wordWrap: "break-word",
+        overflow: "hidden",
+        textOverflow: "ellipsis",
+        alignItems: "stretch",
       }}>
         <Typography variant="body1">
           <div
             dangerouslySetInnerHTML={{
-              __html: shortenContent(props.content,50),
+              __html: shortenContent(props.content, 50),
             }}
           />
         </Typography>
-
-        {/*최대 n자까지만 나타나도록 수정요함. */}
         {/* 이미지에 대해서는 추후 논의 후 추가)*/}
       </Grid>
 
       <Grid item>
-        <Box sx={{ display: "flex", justifyContent: "space-between"}}>
+        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
           {userInfo(props.writer, props.profileImg, props.stuId)}
           {reply_bookmark_views(props)} {/*북마크 onClick 추가 필요*/}
         </Box>
