8fba354af0c3a49f9efcc28f426a0f8ed62733ea
Sauvi9non <xxxdmstj@gmail.com>
2023-05-27 01:47:14 +0900

    FIX: not found 페이지 문구 및 문구 크기 수정

--- src/components/pages/NotFound.tsx
+++ src/components/pages/NotFound.tsx
@@ -1,33 +1,43 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import { Box, Typography } from "@mui/material";
+import { Box, Divider, Typography, Stack } from "@mui/material";
 
 const NotFound = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const timerRef = useRef<number | null>(null);
+  const [countdown, setCountdown] = useState(5);
 
   useEffect(() => {
     timerRef.current = window.setTimeout(() => {
       navigate(location.state?.from || "/");
-    }, 5000);
+    }, countdown * 1000);
+
+    const timer = setInterval(() => {
+      setCountdown((prevState) => prevState - 1);
+    }, 1000);
 
     return () => {
       if (timerRef.current) {
-        window.clearTimeout(timerRef.current);
+        clearTimeout(timerRef.current);
       }
+      clearInterval(timer);
     };
-  }, [navigate]);
+  }, [navigate, location, countdown]);
 
   return (
-    <Box>
+    <Box sx={{ display: "flex", justifyContent: "center", flexDirection: "column" }}>
       <Box>
-        <Typography variant="h6">404 NOT FOUND</Typography>
-        <Typography variant="body1">Oops! 잘못된 접근이에요!</Typography>
-        <Typography variant="body1">
-          5초 후에 이전에 있던 페이지로 돌아갑니다.
-        </Typography>
+        <Typography variant="h1">404 NOT FOUND</Typography>
+        <Divider variant="fullWidth" />
       </Box>
+
+      <Stack>
+        <Typography variant="h3">Oops! 잘못된 접근이에요!</Typography>
+        <Typography variant="h2">
+          {`${countdown}초 후에 메인 페이지로 돌아갑니다.`}
+        </Typography>
+      </Stack>
     </Box>
   );
 };
