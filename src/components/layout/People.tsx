108b9b07fd5e3305f152031b658769c42c4a62f0
Sauvi9non <xxxdmstj@gmail.com>
2023-05-30 01:48:27 +0900

    FIX: 총 인원, 모인 인원 UI, toggleButton 동작 수정

--- src/components/layout/People.tsx
+++ src/components/layout/People.tsx
@@ -34,27 +34,50 @@ const People: React.FC<PeopleProps> = ({
     formState: { errors },
   } = useForm<PeopleInputs>();
 
-  const [partyAlignment, setPartyAlignment] = React.useState<number>(0);
-  const [gatheredAlignment, setGatheredAlignment] = React.useState<number>(0);
+  //여기의 party와 gathered가 사용자가 선택한 값
+  const [party, setParty] = React.useState<number | null>(4);
+  const [gathered, setGathered] = React.useState<number | null>(1);
+
+  const [gatheredButtons, setGatheredButtons] = React.useState<Array<{ value: number; disabled: boolean }>>([
+    { value: 1, disabled: false },
+    { value: 2, disabled: false },
+    { value: 3, disabled: false },
+  ]);
 
   useEffect(() => {
     getParty(partyValue);
     getGathered(gatheredValue);
   }, [partyValue, gatheredValue]);
 
-  const handlePartyChange = (event: React.MouseEvent<HTMLElement>, newAlignment: number) => {
-    setPartyAlignment(newAlignment);
-    getParty(partyAlignment);
+  const handlePartyChange = (event: React.MouseEvent<HTMLElement>, newPartyValue: number | null) => {
+    if (newPartyValue !== null) {
+      setParty(newPartyValue);
+
+      const disabledRange = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].filter((value) => value < newPartyValue);
+      const isDisabled = (value: number) => !disabledRange.includes(value);
+
+      const updatedGatheredButtons = disabledRange.map((value) => ({
+        value,
+        disabled: isDisabled(value),
+      }));
+
+      setGatheredButtons(updatedGatheredButtons);
+      console.log(`사용자가 선택한 party 값: ${party}`);
+      //getParty();
+    }
   };
 
-  const handleGatheredChange = (event: React.MouseEvent<HTMLElement>, newAlignment: number) => {
-    setGatheredAlignment(newAlignment);
-    getGathered(gatheredAlignment);
+  const handleGatheredChange = (event: React.MouseEvent<HTMLElement>, newGatheredValue: number | null) => {
+    if (newGatheredValue !== null) {
+      setGathered(newGatheredValue);
+      console.log(`사용자가 선택한 gathered 값: ${gathered}`);
+      //getGathered();
+    }
   };
 
   return (
-    <Grid item xs={12}>
-      <Stack direction="row" sx={{display:"flex", justifyContent:"space-between"}}>
+    <Grid item container xs={12}>
+      <Grid item xs={12} md={6}>
         <Controller
           name="party"
           control={control}
@@ -65,9 +88,9 @@ const People: React.FC<PeopleProps> = ({
               <ToggleButtonGroup
                 exclusive
                 {...field}
-                value={partyValue}
+                value={party}
                 onChange={handlePartyChange}
-                >
+              >
                 <ToggleButton size="large" value={2}>2</ToggleButton>
                 <ToggleButton size="large" value={3}>3</ToggleButton>
                 <ToggleButton size="large" value={4}>4</ToggleButton>
@@ -88,34 +111,33 @@ const People: React.FC<PeopleProps> = ({
             </FormControl>
           )}
         />
-
+      </Grid>
+      <Grid item xs={12} md={6}>
         <Controller
           name="gathered"
           control={control}
-          rules={{
-            required: true,
-            min: 0,
-            max: partyValue ? partyValue - 1 : undefined,
-          }}
+          rules={{ required: true }}
           render={({ field }) => (
             <FormControl>
+
               <ToggleButtonGroup
+                exclusive
                 {...field}
-                value={gatheredValue}
+                value={gathered}
                 onChange={handleGatheredChange}
-                /* partyValue에 따라 disabled true false */
-                
               >
-                <ToggleButton size="large" value={1}>1</ToggleButton>
-                <ToggleButton size="large" value={2}>2</ToggleButton>
-                <ToggleButton size="large" value={3}>3</ToggleButton>
-                <ToggleButton size="large" value={4}>4</ToggleButton>
-                <ToggleButton size="large" value={5}>5</ToggleButton>
-                <ToggleButton size="large" value={6}>6</ToggleButton>
-                <ToggleButton size="large" value={7}>7</ToggleButton>
-                <ToggleButton size="large" value={8}>8</ToggleButton>
-                <ToggleButton size="large" value={9}>9</ToggleButton>
+                {gatheredButtons.map((button) => (
+                  <ToggleButton
+                    key={button.value}
+                    size="large"
+                    value={button.value}
+                    disabled={button.disabled}
+                  >
+                    {button.value}
+                  </ToggleButton>
+                ))}
               </ToggleButtonGroup>
+
               <FormHelperText>
                 `{`예) 4명팀에서 작성자 본인만 있다면, 현재까지 1명이 모임.`}`
               </FormHelperText>
@@ -130,8 +152,8 @@ const People: React.FC<PeopleProps> = ({
               )}
             </FormControl>
           )}
-        />
-      </Stack>
+        /></Grid>
+
     </Grid>
   );
 };
